3a2da1c7177f14243a11033753d525ed23856733
Jean-LouisH <louis_ml@live.co.uk>
2018-05-04 22:19:55 -0400

    Add project files.

---
+++ ElecCirSim/Constants.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#define FRAME_TIME_MS 16
---
+++ ElecCirSim/Timer.hpp
@@ -0,0 +1,29 @@
+/**
+*                      This file is part of:
+*                          Isle Fight
+*
+* ElecCirSim
+*
+* Defines time data.
+*
+* Copyright (c) 2018 Jean-Louis Haywood. All rights reserved.
+*/
+
+#pragma once
+
+typedef struct Timer
+{
+	double simulation;
+	int cycleStart;
+	int cycleEnd;
+	int cycleDelta;
+	int frameDelay;
+	int frame;
+	Timer() :
+		simulation(0.0),
+		cycleStart(0),
+		cycleEnd(0),
+		cycleDelta(0),
+		frameDelay(0),
+		frame(0) {}
+}Timer;
