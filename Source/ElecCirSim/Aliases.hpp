9cc6cca58ddaca83e31f9a1194baad8cae4b5472
Jean-LouisH <Louis_Ml@live.co.uk>
2019-01-18 15:14:46 -0400

    Added aliases for referencing indices in code as integers

---
+++ Source/ElecCirSim/Aliases.hpp
@@ -0,0 +1,14 @@
+/**
+*                      This file is part of:
+*                          ElecCirSim
+*
+*
+* Copyright (c) 2018-2019 Jean-Louis Haywood. All rights reserved.
+*/
+
+#pragma once
+
+#include <stdint.h>
+
+using ComponentIndex = uint32_t;
+using TerminalIndex = uint32_t;
