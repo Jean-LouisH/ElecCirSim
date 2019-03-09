a709cdd875d2a6c86d70c87180018991b708fb1d
Jean-LouisH <Louis_Ml@live.co.uk>
2019-03-09 16:28:20 -0400

    Prevents looping if startup error occurs

--- Source/ElecCirSim/main.cpp
+++ Source/ElecCirSim/main.cpp
@@ -1,6 +1,7 @@
 #include <SDL.h>
 
 #include "ElecCirSim.hpp"
+#include "Application.hpp"
 #include "Core.hpp"
 
 #include "Constants.hpp"
@@ -9,11 +10,16 @@
 int main(int argc, char* argv[])
 {
 	ElecCirSim::Core* core = new ElecCirSim::Core();
+	ElecCirSim::Application* application = new ElecCirSim::Application();
+
+	core->filepath = argv[0];
 
 	ElecCirSim::initialize(core);
-	ElecCirSim::loop(core);
-	ElecCirSim::shutdown(core);
+	if (core->state != ElecCirSim::SHUTDOWN)
+		ElecCirSim::loop(core, application);
+	ElecCirSim::shutdown(core, application);
 
+	delete application;
 	delete core;
 
 	return 0;
